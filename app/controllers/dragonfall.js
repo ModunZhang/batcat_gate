b764878d86207e9772316cf5575d7947de2a248c
Modun Zhang <modunzhang@gmail.com>
2015-08-22 15:49:16 +0800

    debug

--- app/controllers/dragonfall.js
+++ app/controllers/dragonfall.js
@@ -12,35 +12,42 @@ var router = express.Router();
 module.exports = router;
 
 var Config = {
-    DevUpdateServer: '54.223.172.65:3000',
-    DevGateServer: '54.223.172.65:3011',
-    AppleUpdateServer: '54.223.202.136:3000',
-    AppleGateServer: '54.223.202.136:3011',
-    ReleaseUpdateServer: '54.223.166.65:3000',
-    ReleaseGateServer: '54.223.166.65:3011',
-    CurrentVersion: '1.01',
-    AppleVersion: '1.1.1'
+  DevUpdateServer: '54.223.172.65:3000',
+  DevGateServer: '54.223.172.65:3011',
+  AppleUpdateServer: '54.223.202.136:3000',
+  AppleGateServer: '54.223.202.136:3011',
+  ReleaseUpdateServer: '54.223.166.65:3000',
+  ReleaseGateServer: '54.223.166.65:3011',
+  CurrentVersion: '1.01',
+  AppleVersion: '1.1.1'
 };
 
 router.get('/query-entry', function (req, res) {
-    var query = req.query;
-    var version = query.version;
-    var env = query.env;
+  var query = req.query;
+  var version = query.version;
+  var env = query.env;
 
-    if (!_.contains(consts.GameEnv, env)) return res.json({code: 500, message: "env 不合法"});
-    if (env === consts.GameEnv.Development) return res.json({
-        code: 200,
-        data: {updateServer: Config.DevUpdateServer, gateServer: Config.DevGateServer}
+  if (!_.contains(consts.GameEnv, env)) return res.json({code: 500, message: "env 不合法"});
+  //if (env === consts.GameEnv.Development) {
+    return res.json({
+      code: 200,
+      data: {updateServer: Config.DevUpdateServer, gateServer: Config.DevGateServer}
     });
-    if (env === consts.GameEnv.Production) {
-        if (version === Config.CurrentVersion) return res.json({
-            code: 200,
-            data: {updateServer: Config.ReleaseUpdateServer, gateServer: Config.ReleaseGateServer}
-        });
-        if (version === Config.AppleVersion) return res.json({
-            code: 200,
-            data: {updateServer: Config.AppleUpdateServer, gateServer: Config.AppleGateServer}
-        });
-    }
-    res.sendStatus(400);
+  //}
+  //
+  //if (env === consts.GameEnv.Production) {
+  //  if (version === Config.CurrentVersion) {
+  //    return res.json({
+  //      code: 200,
+  //      data: {updateServer: Config.ReleaseUpdateServer, gateServer: Config.ReleaseGateServer}
+  //    });
+  //  }
+  //  if (version === Config.AppleVersion) {
+  //    return res.json({
+  //      code: 200,
+  //      data: {updateServer: Config.AppleUpdateServer, gateServer: Config.AppleGateServer}
+  //    });
+  //  }
+  //}
+  //res.sendStatus(400);
 });
